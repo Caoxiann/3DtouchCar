f2a06b3abd211b38f9fe8d578ceeeb5d71c793d5
陈浩贤 <826919861@qq.com>
2016-11-07 13:18:10 +0800

    can turn in an angle now

--- 3DTouchCar/_DTouchCar.ino
+++ 3DTouchCar/_DTouchCar.ino
@@ -64,26 +64,27 @@ AF_DCMotor motorLF(3);//左前轮
 AF_DCMotor motorLB(4);//左后轮
 
 int carSpeed; //小车的速度
-int status;
+int status;//小车状态
 float RFzw=1,RBzw=1,LFzw=0.9,LBzw=0.9;//四个轮胎的速度系数
-
+float trunRatio;//转向系数
 
 void goBackward(int speed);
 void trun(int angle);
 void stop();
 void goForward(int speed);
+void setSpeed();
+void turnAround(int direction);//0向左转，1向右转
 // Add setup code
 void setup()
 {
     
     Serial.begin(9600);
+    
     // turn on motor
-    carSpeed=100;
+    carSpeed=150;
     status=0;
-    motorRB.run(RELEASE);
-    motorRF.run(RELEASE);
-    motorLB.run(RELEASE);
-    motorLF.run(RELEASE);
+    stop();
+    
 }
 
 // Add loop code
@@ -93,9 +94,7 @@ void loop()
         status=Serial.read();
         status=status-'0';
         Serial.print("status:");
-        
         Serial.print(status);
-        
         Serial.print("\n");
     }
     switch (status) {
@@ -103,18 +102,47 @@ void loop()
             stop();
             break;
         case 1:
-            goForward(255);
+            goForward(100);
             break;
         case 2:
-            goBackward(255);
+            goBackward(100);
+            break;
+        case 3:
+            trun(75);
+            break;
+        case 4:
+            trun(-75);
+            break;
+        case 5:
+            turnAround(0);
+            break;
+        case 6:
+            turnAround(1);
             break;
         default:
             break;
     }
 
+
 }
 
 void trun(int angle){
+    RFzw=1;
+    RBzw=1;
+    LFzw=1;
+    LBzw=1;
+    if (angle>=0) {
+        trunRatio=1-angle/90.0;
+        RBzw=trunRatio*RBzw;
+        RFzw=trunRatio*RFzw;
+        
+    }else{
+        trunRatio=1+angle/90.0;
+        LBzw=trunRatio*LBzw;
+        LFzw=trunRatio*LFzw;
+    }
+    
+    setSpeed();
     
 }
 
@@ -126,10 +154,12 @@ void goBackward(int speed){
     motorLB.run(BACKWARD);
     motorLF.run(BACKWARD);
     
-    motorRF.setSpeed(carSpeed*RFzw);
-    motorLF.setSpeed(carSpeed*LFzw);
-    motorLB.setSpeed(carSpeed*LBzw);
-    motorRB.setSpeed(carSpeed*RBzw);
+    RFzw=0.9;
+    RBzw=0.9;
+    LFzw=1;
+    LBzw=1;
+    
+    setSpeed();
 }
 
 void stop(){
@@ -147,7 +177,31 @@ void goForward(int speed){
     motorLB.run(FORWARD);
     motorLF.run(FORWARD);
     
+    RFzw=1;
+    RBzw=1;
+    LFzw=0.9;
+    LBzw=0.9;
+    setSpeed();
+}
+
+void setSpeed(){
     motorRF.setSpeed(carSpeed*RFzw);
     motorLF.setSpeed(carSpeed*LFzw);
     motorLB.setSpeed(carSpeed*LBzw);
-    motorRB.setSpeed(carSpeed*RBzw);}
+    motorRB.setSpeed(carSpeed*RBzw);
+}
+
+void turnAround(int direction){
+    if (direction==0) {
+        motorLB.run(BACKWARD);
+        motorLF.run(BACKWARD);
+        motorRB.run(FORWARD);
+        motorRF.run(FORWARD);
+    }else{
+        motorRB.run(BACKWARD);
+        motorRF.run(BACKWARD);
+        motorLB.run(FORWARD);
+        motorLF.run(FORWARD);
+    }
+    setSpeed();
+}
